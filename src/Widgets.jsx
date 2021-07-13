4afcac596b6d8dfafb8acd368f9c8a42731f40dc
dingus45191 <mubashirhasan716@gmail.com>
2021-07-13 21:37:11 +0530

    bugged out

--- src/Widgets.jsx
+++ src/Widgets.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React from "react";
 import "./Widgets.css";
 
 function Widgets() {
-    return (
-        <div className='widgets'>
-            <iframe
-                src="https://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fwfuvpage&amp;width=340&amp;height=1500&amp;colorscheme=light&amp;show_faces=true&amp;border_color&amp;stream=true&amp;header=true"
-                style={{border: 'none',overflow: 'hidden'}}
-                scrolling="no" frameBorder="0"
-                width="340"
-                height="100%"
-                allowTransparency="true"></iframe>
-        </div>
-    );
+  return (
+    <div className="widgets">
+      <iframe
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fwired%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        style={{ border: "none", overflow: "hidden", marginRight: "10px" }}
+        scrolling="no"
+        frameBorder="0"
+        width="340"
+        height="100%"
+        frameborder="0"
+        allowfullscreen="true"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        title="WIRED"
+      ></iframe>
+    </div>
+  );
 }
 export default Widgets;
